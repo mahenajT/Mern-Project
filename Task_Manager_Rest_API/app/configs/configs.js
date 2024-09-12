67cc479ca75b74f4d46a788b435437c9b0c151c3
mahenajT <mahenaj200815@gmail.com>
2024-09-12 09:12:53 +0600

    Project Completed

--- Task_Manager_Rest_API/app/configs/configs.js
+++ Task_Manager_Rest_API/app/configs/configs.js
@@ -1,15 +1,15 @@
 // configuration file
 
 export const PORT = 5050;
-export const DATABASE_URL = "mongodb+srv://nasrin:nasrin1024@mernbatch-6.zsj87.mongodb.net/TaskManagerAPI";
+export const DATABASE_URL = "";
 export const JWT_KEY = "ABCD";
 export const JWT_EXPIRE_TIME = 30 * 24 * 60 * 60;
 
-export const EMAIL_HOST = "mail.teamrabbil.com";
-export const EMAIL_PORT = 25;
-export const EMAIL_SECURITY = false;
-export const EMAIL_USER = "info@teamrabbil.com";
-export const EMAIL_PASSWORD = "~sR4[bhaC[Qs";
+export const EMAIL_HOST = "";
+export const EMAIL_PORT = 0;
+export const EMAIL_SECURITY = 123;
+export const EMAIL_USER = "";
+export const EMAIL_PASSWORD = "";
 export const EMAIL_UN_AUTHORIZATION = false;
 
 export const WEB_CACHE = false;
